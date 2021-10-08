7c8d0861e479e3620cde3daed6e0359c0a518dbe
Hazel Eythorsdottir <hazeleythorsdottir@gmail.com>
2021-10-08 07:34:01 -0400

    Create start / skeleton of ATA PIO driver. Driver not useable yet

--- include/drivers/ata/ata.h
+++ include/drivers/ata/ata.h
@@ -1,8 +1,49 @@
 #pragma once
 
+#include <bool.h>
+
 /* Very basic ATA driver. Includes basic loading and reading from drive in
  * LBA format 
  */
 
-void driver_ata_init ( void );
-void driver_ata_lba_to_chs ( int lba, int *head, int *track, int *sector );
+/* Data that's only useful to ata driver implementations
+ * Hidden behind define as to not clog up global namespace*/
+#ifdef DRIVER_ATA_IMPORT_STRUCTS
+
+#define DRIVER_ATA_IO_BASE_PRI 0x1F0
+#define DRIVER_ATA_IO_BASE_SEC 0x3F6
+
+enum driver_ata_port
+{
+    DATA = 0,
+    ERR,
+    SECTOR_COUNT,
+    LBA_LO,
+    LBA_MID,
+    LBA_HI,
+    DRIVE,
+    COMMAND
+};
+
+#endif
+
+void  driver_ata_init ( void ) ;
+
+/* Send the "Identify" command to the acpi device and record 
+ * the information returned.
+ * If pri is true, query the master bus. Otherwise,
+ * query the slave.
+ */ 
+void _driver_ata_identify ( bool pri );
+
+/* Select a master or slave bus.
+ * Will select five times in order to add a 400ns delay
+ * If pri is true, select the master bus. Otherwise,
+ * select the slave.
+ */ 
+void _driver_ata_select ( bool pri );
+
+void  driver_ata_debug_print ( void );
+
+
+void  driver_ata_lba_to_chs ( int lba, int *head, int *track, int *sector );
--- source/drivers/ata/ata.c
+++ source/drivers/ata/ata.c
@@ -1,11 +1,40 @@
-#include "boot/util.h"
+#define DRIVER_ATA_IMPORT_STRUCTS
 #include <drivers/ata/ata.h>
-#include <drivers/serial/serial.h>
+#include <drivers/io/io.h>
 #include <boot/tty/tty.h>
 
 extern char BOOT_DRIVEINFO[2];
 
-void driver_ata_init()
+void driver_ata_init ( void )
+{
+    _driver_ata_identify(TRUE);
+}
+
+void _driver_ata_identify ( bool pri )
+{
+    _driver_ata_select(pri);
+    // Send identify command
+    outb(DRIVER_ATA_IO_BASE_PRI + COMMAND, 0xEC);
+
+    // Set sec->lba_hi to 0
+    outb(DRIVER_ATA_IO_BASE_PRI + SECTOR_COUNT, 0);
+    outb(DRIVER_ATA_IO_BASE_PRI + LBA_LO,       0);
+    outb(DRIVER_ATA_IO_BASE_PRI + LBA_MID,      0);
+    outb(DRIVER_ATA_IO_BASE_PRI + LBA_HI,       0);
+}
+
+void _driver_ata_select ( bool pri ) 
+{
+    char port = pri ? 0xA0 : 0xB0;
+    // Send identify command to primary bus 
+    // Query primary port
+    for (int i = 0; i < 5; i++)
+    {
+       outb(DRIVER_ATA_IO_BASE_PRI + DRIVE, port);
+    }
+}
+
+void driver_ata_debug_print ( void )
 {
     tty_print("Hard drive info:\n");
     tty_print("   Number of heads   : %d\n", (int)BOOT_DRIVEINFO[0]);
